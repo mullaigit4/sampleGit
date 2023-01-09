37e27d718fbbdbe763b6a7b177969fbcb9c589f9
mullaigit4 <mullahrtr2@gmail.com>
2023-01-09 14:08:20 +0530

    methods added

---
+++ src/com/gittask/GitTask.java
@@ -0,0 +1,14 @@
+package com.gittask;
+
+public class GitTask {
+	public void tc1() {
+		System.out.println("tamil");
+	}
+	public void tc2() {
+		System.out.println("eng");
+	}
+	public void tc3() {
+		System.out.println("malayalam");
+	}
+
+}
